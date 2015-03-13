4c53c4cf9d3dd539a6fa1cb517340f2368fa7f05
Lucas <lucas@agtlucas.com>
2015-03-13 08:05:13 -0300

    Update styles.js

---
+++ gulp/styles.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('plumber');
+var prefix = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var refresh = require('gulp-livereload');
+var rename = require('gulp-rename');
