8d9670b11ed9f8aa35a0d91c6042010278c3f439
Lucas <lucas@agtlucas.com>
2015-03-13 08:12:35 -0300

    Update styles.js

--- gulp/styles.js
+++ gulp/styles.js
@@ -5,3 +5,22 @@ var prefix = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
 var refresh = require('gulp-livereload');
 var rename = require('gulp-rename');
+
+module.exports = function () {
+
+  var styles = gulp.src('public/src/scss/app.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(prefix('last 1 version', '> 1%', 'ie 8'))
+    .on('error', notify.onError())
+    .pipe(gulp.dest('public/dist/css'))
+    .on('end', function () {
+      if (global.lrserver) {
+        gulp.src('public/dist/css/app.css')
+          .pipe(refresh(global.lrserver));
+      }
+    });
+
+    return styles;
+
+};
