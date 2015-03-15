94dd7ae1136e27d9687a78e1991f790799a9d2a2
Lucas <lucas@agtlucas.com>
2015-03-15 10:43:56 -0300

    Update scripts.js

---
+++ gulp/prod/scripts.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var header = require('gulp-header');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var refresh = require('gulp-livereload');
+var rename = require('gulp-rename');
+var transform = require('vynil-transform');
+var multipipe = require('multipipe');
+var flow = require('gulp-flowtype');
+var es6ify = require('e6ify');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var exec = require('child_process').exec;
+var flowToJshint = require('flow-to-jshint');
+var stylish = require('jshint-stylish');
+var reporter = require(stylish).reporter;
+var bannerTop = '/* @flow */' + '\n';
