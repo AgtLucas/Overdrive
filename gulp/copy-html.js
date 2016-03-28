b0ca0b605007591a54c623b2c91bdbf1e2c4a7a6
Lucas <AgtLucas@users.noreply.github.com>
2016-03-28 19:22:59 -0300

    Update copy-html.js

--- gulp/copy-html.js
+++ gulp/copy-html.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var refresh = require('gulp-livereload');
-var preprocess = require('gulp-preprocess');
+var gulp = require('gulp')
+var refresh = require('gulp-livereload')
+var preprocess = require('gulp-preprocess')
 
 module.exports = function () {
   return gulp.src('public/src/**/*.html')
     .pipe(preprocess({context: {dev: true}}))
     .pipe(gulp.dest('public/dist'))
-    .pipe(refresh(global.lrserver));
-};
+    .pipe(refresh(global.lrserver))
+}
