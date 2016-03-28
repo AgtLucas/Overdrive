3f26b20d5490af5048e1e643aa023b22de129173
Lucas <AgtLucas@users.noreply.github.com>
2016-03-28 19:23:41 -0300

    Update copy-libs.js

--- gulp/copy-libs.js
+++ gulp/copy-libs.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 
 module.exports = function () {
   gulp.src('public/src/fonts/**/*')
-    .pipe(gulp.dest('public/dist/fonts'));
+    .pipe(gulp.dest('public/dist/fonts'))
 
   gulp.src('public/src/img/**/*')
-    .pipe(gulp.dest('public/dist/img'));
+    .pipe(gulp.dest('public/dist/img'))
 
   gulp.src('public/libs/react/react-with-addons.js')
-    .pipe(gulp.dest('public/dist/js'));
-};
+    .pipe(gulp.dest('public/dist/js'))
+}
